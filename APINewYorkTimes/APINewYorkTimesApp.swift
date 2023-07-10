cdd4d87b385d5fe13ebdc6180b2f5f872df959e2
Vladimir Lukyanenko <>
2023-07-10 18:31:54 +0300

    first commit

---
+++ APINewYorkTimes/APINewYorkTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  APINewYorkTimesApp.swift
+//  APINewYorkTimes
+//
+//  Created by Vladimir Lukyanenko on 10.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct APINewYorkTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
